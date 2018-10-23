a6b3250e241ae321f8f648ba2b72acbb73ab0ab6
Zhenia Steger <pcgamr91@gmail.com>
2018-10-22 19:17:13 -0700

    Added more tests

--- test/routes/videos-test.js
+++ test/routes/videos-test.js
@@ -50,7 +50,7 @@ describe('GET /videos/:id', () => {
       url: generateRandomUrl('www.youtube.com'),
     };
 
-    const response = await request(app).get('/videos/${video.id}');
+    const response = await request(app).get(`/videos/${videoToSave.id}`);
 
     const text = parseTextFromHTML(response.text, 'body');
     assert.include(text, videoToSave.title);
@@ -92,5 +92,117 @@ describe('POST /videos', () => {
 
       const video = await Video.findOne({});
       assert.include(video, videoToSave);
+  });
+
+  describe('when the title is missing', () => {
+    it('does not save the video', async () => {
+      const videoToSave = {
+        title: '',
+        description: 'This is a test video',
+        url: generateRandomUrl('www.youtube.com')
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+        const video = await Video.find({});
+        assert.equal(video.length, 0);
+    });
+
+    it('responds with a 400 error', async () =>{
+      const videoToSave = {
+        title: '',
+        description: 'This is a test video',
+        url: generateRandomUrl('www.youtube.com')
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+        const video = await Video.find({});
+        assert.equal(response.status, 400);
+    });
+
+    it('renders the video form', async () => {
+      const videoToSave = {
+        title: '',
+        description: 'This is a test video',
+        url: generateRandomUrl('www.youtube.com')
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+        const title = queryHTML(response.text, '[name="title"]');
+        assert.ok(title, 'could not find `title` input');
+    });
+
+    it('renders the validation error message', async () =>{
+      const videoToSave = {
+        title: '',
+        description: 'This is a test video',
+        url: generateRandomUrl('www.youtube.com')
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+        const text = parseTextFromHTML(response.text, 'body');
+        assert.include(text, 'title is required');
+    });
+
+    it('preserves the rest of the fields', async () =>{
+      const videoToSave = {
+        title: '',
+        description: 'This is a test video',
+        url: generateRandomUrl('www.youtube.com')
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+      const description = queryHTML(response.text, '[name="description"]');
+      const url = queryHTML(response.text, '[name="url"]');
+      assert.equal(description.value, videoToSave.Description);
+      assert.equal(url.value, videoToSave.url);
+    });
+  });
+
+  describe('when the url is missing', () => {
+    it('renders the validation erroe message', async () => {
+      const videoToSave = {
+        title: 'Some Title',
+        description: 'This is a test video',
+        url: ''
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+        const text = parseTextFromHTML(response.text, 'body');
+        assert.include(text, 'a url is required');
+    });
+    it('preserves the rest of the fields', async () =>{
+      const videoToSave = {
+        title: 'Some Title',
+        description: 'This is a test video',
+        url: ''
+      }
+      const response = await request(app)
+        .post('/videos')
+        .type('form')
+        .send(videoToSave);
+
+      const title = queryHTML(response.text, '[name="title"]');
+      const description = queryHTML(response.text, '[name="description"]');
+      assert.equal(description.value, videoToSave.title);
+      assert.equal(description.value, videoToSave.description);
+    });
   });
 });
